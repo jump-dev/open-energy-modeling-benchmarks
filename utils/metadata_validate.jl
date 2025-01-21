4eaad61075356a66aa9af327b5696386f876eeef
joaquimg <joaquimdgarcia@gmail.com>
2025-01-20 22:13:14 -0800

    format

--- utils/metadata_validate.jl
+++ utils/metadata_validate.jl
@@ -3,18 +3,20 @@ cd(@__DIR__)
 import TOML
 
 function validate()
-
     instances_path = joinpath(pwd(), "..", "instances")
 
-    metadata = TOML.parsefile(joinpath(instances_path, "metadata.toml"))["instance"]
+    metadata =
+        TOML.parsefile(joinpath(instances_path, "metadata.toml"))["instance"]
 
-    files = readdir(instances_path, sort=true, join=false)
+    files = readdir(instances_path; sort = true, join = false)
 
     # remove ignored files
     to_remove_name_start = ["metadata.toml", "."]
     to_remove_indices = Int[]
     for (i, file) in enumerate(files)
-        if any(startswith(file, name_start) for name_start in to_remove_name_start)
+        if any(
+            startswith(file, name_start) for name_start in to_remove_name_start
+        )
             push!(to_remove_indices, i)
         end
     end
@@ -28,7 +30,10 @@ function validate()
         for (j, name) in enumerate(metadata_names)
             if i != j
                 if metadata_names[i] == metadata_names[j]
-                    println("Duplicate file name in metadata.toml: ", metadata_names[i])
+                    println(
+                        "Duplicate file name in metadata.toml: ",
+                        metadata_names[i],
+                    )
                     failed_to_validate = true
                 end
             end
@@ -61,13 +66,25 @@ function validate()
         end
         for key in data_keys
             if !haskey(data, key)
-                println("Key \"", key, "\" not found for: \"", data["name"], "\"")
+                println(
+                    "Key \"",
+                    key,
+                    "\" not found for: \"",
+                    data["name"],
+                    "\"",
+                )
                 failed_to_validate = true
             end
         end
         for key in keys(data)
             if !(key in data_keys)
-                println("Key \"", key, "\" not not expected for: \"", data["name"], "\"")
+                println(
+                    "Key \"",
+                    key,
+                    "\" not not expected for: \"",
+                    data["name"],
+                    "\"",
+                )
                 failed_to_validate = true
             end
         end
